efbef6cdb41b17185d9d3f417018993845f8d686
Ko Heeyeun <koheeyeun96@gmail.com>
2025-03-24 21:11:11 +0900

    chore: 이슈, PR 템플릿 설정

--- src/home/pages/HomePage.tsx
+++ src/home/pages/HomePage.tsx
@@ -1,8 +1,4 @@
 import React, { useState } from "react";
-import menu1 from "../../assets/menu1.png";
-import menu2 from "../../assets/menu2.png";
-import menu3 from "../../assets/menu3.png";
-import menu4 from "../../assets/menu4.png";
 
 export default function HomePage() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -14,15 +10,7 @@ export default function HomePage() {
   return (
     <div className="h-full flex flex-col bg-[#EDEDED]">
       <div className="w-16 h-full bg-[#FFFFFF] border-r-[#BBBBBB] border-r-[0.5px]">
-        <div className="h-full flex flex-col items-start p-2 space-y-2 gap-7 mt-3">
-          {/* <button onClick={toggleDrawer}>
-            <img src={menu1} alt="menu1" className="w-12 h-12" />
-          </button>
-
-          <img src={menu2} alt="menu2" className="w-12 h-12" />
-          <img src={menu3} alt="menu3" className="w-12 h-12" />
-          <img src={menu4} alt="menu4" className="w-12 h-12" /> */}
-        </div>
+        <div className="h-full flex flex-col items-start p-2 space-y-2 gap-7 mt-3"></div>
       </div>
     </div>
   );
