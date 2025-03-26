3e71c7ca51ae50c1020bb4d3c0c8f530726fc625
Ko Heeyeun <koheeyeun96@gmail.com>
2025-03-26 23:20:34 +0900

    feat: 버튼 hover 액션 추가
    feat: 버튼 hover 액션 추가
    
    사이드바 버튼 hover 액션 추가
    
    Res: #3

--- src/main/pages/HomePage.tsx
+++ src/main/pages/HomePage.tsx
@@ -17,43 +17,95 @@ export default function HomePage() {
       <div className='h-full w-[370px] bg-transparent relative flex'>
         <div className='w-[70px] h-full bg-[#FFFFFF] border-r-[#BBBBBB] border-r-[0.5px] z-10'>
           <div className='h-full flex flex-col items-start p-[6px] space-y-1 gap-3 mt-1'>
-            {menuType.map((type) => (
-              <button
-                key={type}
-                className={`w-[58px] h-[58px] rounded-[8px] ${selectedType === type ? 'bg-[#C9DCF9]' : 'bg-[#FFFFFF]'} hover:bg-[#C9DCF9] flex flex-col justify-center pt-1`}
-                onClick={() => toggleDrawer(type)}
-              >
-                <div className='w-[28px] h-[28px] rounded-sm border-2 border-[#3A7DE8] self-center flex flex-col justify-center'>
-                  {type === 'text' ? (
-                    <Baseline size='18' color='#3A7DE8' strokeWidth='2.5' className='self-center' />
-                  ) : type === 'image' ? (
-                    <Image size='18' color='#3A7DE8' strokeWidth='2.5' className='self-center' />
-                  ) : type === 'video' ? (
-                    <Play size='18' color='#3A7DE8' strokeWidth='2.5' className='self-center' />
-                  ) : type === 'audio' ? (
+            {menuType.map((type) =>
+              type === 'text' ? (
+                <button
+                  key={type}
+                  className={`w-[58px] h-[58px] rounded-[8px] ${selectedType === type ? 'bg-[#C9DCF9]' : 'bg-[#FFFFFF]'} hover:bg-[#C9DCF9] flex flex-col justify-center pt-1 group`}
+                  onClick={() => toggleDrawer(type)}
+                >
+                  <div
+                    className={`w-[28px] h-[28px] rounded-sm border-2 ${selectedType === type ? 'border-[#3A7DE8]' : 'border-[#808080]'} self-center flex flex-col justify-center group-hover:border-[#3A7DE8]`}
+                  >
+                    <Baseline
+                      size='18'
+                      strokeWidth='2.5'
+                      className={`self-center group-hover:bottom-0 ${selectedType == type ? 'text-[#3A7DE8]' : 'text-[#808080]'} group-hover:text-[#3A7DE8]`}
+                    />
+                  </div>
+                  <div
+                    className={`font-[Noto Sans] font-semibold text-center text-[12px] ${selectedType == type ? 'text-[#3A7DE8]' : 'text-[#808080]'} group-hover:text-[#3A7DE8]`}
+                  >
+                    텍스트
+                  </div>
+                </button>
+              ) : type === 'image' ? (
+                <button
+                  key={type}
+                  className={`w-[58px] h-[58px] rounded-[8px] ${selectedType === type ? 'bg-[#FFF1D1]' : 'bg-[#FFFFFF]'} hover:bg-[#FFF1D1] flex flex-col justify-center pt-1 group`}
+                  onClick={() => toggleDrawer(type)}
+                >
+                  <div
+                    className={`w-[28px] h-[28px] rounded-sm border-2 ${selectedType === type ? 'border-[#FFBD1E]' : 'border-[#808080]'} self-center flex flex-col justify-center group-hover:border-[#FFBD1E]`}
+                  >
+                    <Image
+                      size='18'
+                      strokeWidth='2.5'
+                      className={`self-center group-hover:bottom-0 ${selectedType == type ? 'text-[#3A7DE8]' : 'text-[#808080]'} group-hover:text-[#FFBD1E]`}
+                    />
+                  </div>
+                  <div
+                    className={`font-[Noto Sans] font-semibold text-center text-[12px] ${selectedType == type ? 'text-[#FFBD1E]' : 'text-[#808080]'} group-hover:text-[#FFBD1E]`}
+                  >
+                    이미지
+                  </div>
+                </button>
+              ) : type === 'video' ? (
+                <button
+                  key={type}
+                  className={`w-[58px] h-[58px] rounded-[8px] ${selectedType === type ? 'bg-[#FBE6F7]' : 'bg-[#FFFFFF]'} hover:bg-[#FBE6F7] flex flex-col justify-center pt-1 group`}
+                  onClick={() => toggleDrawer(type)}
+                >
+                  <div
+                    className={`w-[28px] h-[28px] rounded-sm border-2 ${selectedType === type ? 'border-[#EF9EE1]' : 'border-[#808080]'} self-center flex flex-col justify-center group-hover:border-[#EF9EE1]`}
+                  >
+                    <Play
+                      size='18'
+                      strokeWidth='2.5'
+                      className={`self-center group-hover:bottom-0 ${selectedType == type ? 'text-[#EF9EE1]' : 'text-[#808080]'} group-hover:text-[#EF9EE1]`}
+                    />
+                  </div>
+                  <div
+                    className={`font-[Noto Sans] font-semibold text-center text-[12px] ${selectedType == type ? 'text-[#EF9EE1]' : 'text-[#808080]'} group-hover:text-[#EF9EE1]`}
+                  >
+                    비디오
+                  </div>
+                </button>
+              ) : type === 'audio' ? (
+                <button
+                  key={type}
+                  className={`w-[58px] h-[58px] rounded-[8px] ${selectedType === type ? 'bg-[#B2CBC0]' : 'bg-[#FFFFFF]'} hover:bg-[#B2CBC0] flex flex-col justify-center pt-1 group`}
+                  onClick={() => toggleDrawer(type)}
+                >
+                  <div
+                    className={`w-[28px] h-[28px] rounded-sm border-2 ${selectedType === type ? 'border-[#337758]' : 'border-[#808080]'} self-center flex flex-col justify-center group-hover:border-[#337758]`}
+                  >
                     <AudioLines
                       size='18'
-                      color='#3A7DE8'
                       strokeWidth='2.5'
-                      className='self-center'
+                      className={`self-center group-hover:bottom-0 ${selectedType == type ? 'text-[#337758]' : 'text-[#808080]'} group-hover:text-[#337758]`}
                     />
-                  ) : (
-                    <></>
-                  )}
-                </div>
-                <div className='font-[Noto Sans] font-semibold text-center text-[12px] text-[#3A7DE8]'>
-                  {type === 'text'
-                    ? '텍스트'
-                    : type === 'image'
-                      ? '이미지'
-                      : type === 'video'
-                        ? '비디오'
-                        : type === 'audio'
-                          ? '오디오'
-                          : ''}
-                </div>
-              </button>
-            ))}
+                  </div>
+                  <div
+                    className={`font-[Noto Sans] font-semibold text-center text-[12px] ${selectedType == type ? 'text-[#337758]' : 'text-[#808080]'} group-hover:text-[#337758]`}
+                  >
+                    오디오
+                  </div>
+                </button>
+              ) : (
+                <></>
+              ),
+            )}
           </div>
         </div>
         <SideMenu type={selectedType} />
