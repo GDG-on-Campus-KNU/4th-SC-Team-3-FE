8623d236c8699856db7fecfd13bb8bf495a88c63
Ko Heeyeun <koheeyeun96@gmail.com>
2025-04-05 22:47:27 +0900

    chore: prettier tailwindcss 플러그인 순서 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "@/app/routes/Router";
+import { BrowserRouter } from 'react-router-dom';
+
+import { Router } from '@/app/routes/Router';
 
 export default function App() {
   return (
