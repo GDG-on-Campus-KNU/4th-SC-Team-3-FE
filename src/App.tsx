552d7681a6a3d7e6ada37f9b208947a0e846fa9a
Ko Heeyeun <koheeyeun96@gmail.com>
2025-05-10 22:15:39 +0900

    refactor: 토스트 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { Router } from '@/app/routes/Router';
-import { ToastProvider } from '@/global/ui/toast';
-import { Toaster } from '@/global/ui/toaster';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -10,12 +8,9 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <BrowserRouter>
-          <Router />
-          <Toaster />
-        </BrowserRouter>
-      </ToastProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
