09396c411443548176eb9ee07a8aa8ab15072f29
Ko Heeyeun <koheeyeun96@gmail.com>
2025-05-12 01:45:03 +0900

    fix: 대시보드 파이피 설명 수정

--- src/dashboard/components/DashBoardHeader.tsx
+++ src/dashboard/components/DashBoardHeader.tsx
@@ -4,7 +4,7 @@ import ImagePiper from '@/assets/dashboard/img-piper.png';
 import Logo from '@/assets/logo.svg';
 
 export const DashBoardHeader = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <div className='relative h-[32%] w-full overflow-hidden bg-[#EDEDED]'>
       <div className='absolute left-0 flex h-full w-full flex-col items-center justify-center bg-[#EDEDED]'>
@@ -13,10 +13,19 @@ export const DashBoardHeader = () => {
           <h1 className='ml-4 text-7xl font-bold tracking-widest text-[#4D4D4D]'>PIPY</h1>
         </div>
         <div className='mt-3 text-lg'>
-          <p className='text-[#808080]'> {t('dashboard.title.top')}</p>
+          <p className='text-[#808080]'>{t('dashboard.title.top')}</p>
           <div className='flex items-center'>
-            <p className='font-bold text-[#3a7deb]'> {t('dashboard.title.middle')}</p>
-            <p className='text-[#808080]'>{t('dashboard.title.bottom')}</p>
+            { i18n.language === 'en' ?
+              <>
+                <p className='font-bold text-[#3a7deb]'>{t('dashboard.title.middle')}</p>
+                <div className='border-t border-[#808080] mx-2 w-3'></div>
+                <p className='text-[#808080]'>{t('dashboard.title.bottom')}</p>
+              </>
+              : 
+              <>
+                <p className='font-bold text-[#3a7deb]'>{t('dashboard.title.middle')}</p>
+                <p className='text-[#808080]'>{t('dashboard.title.bottom')}</p>
+              </> }
           </div>
         </div>
       </div>
